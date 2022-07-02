57e8360c11c3a5c49f16c42e1b1dcca65f28aee1
Camilo Serna Restrepo <camilosernar@outlook.com>
2022-07-02 10:53:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -250,5 +250,4 @@ def update_table(active_page):
 ## ----------------------------------------------------------------------------
 ## start server
 if __name__ == '__main__':
-    app.run_server(debug=False)
-    #app.run_server(debug=False, port=8050)
+    app.run_server(debug=True)
