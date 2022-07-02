cc89b9b5aa13499910c8cb46cd08af7dc7d14f3f
Camilo Serna Restrepo <camilosernar@outlook.com>
2022-07-02 10:53:04 -0700

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import plotly.express as px
 ## ----------------------------------------------------------------------------
 ## app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 ## Start application
-app = dash.Dash(__name__)
+app = dash.Dash()
 server = app.server
 
 
